3f501c2ae97042d60a5bc1b6614ebf1a0b2244dd
18thday <day18th@yandex.ru>
2024-07-21 09:40:46 +0500

    Fix: atomic access to counter

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
+#include <atomic>
+#include <chrono>
 #include <iostream>
 #include <thread>
-#include <chrono>
 
-void add_client(size_t& client_count, size_t max_count) {
+void add_client(std::atomic<size_t>& client_count, size_t max_count) {
     std::cout << "add_client start" << std::endl;
     for (size_t i = 0; i < max_count; ++i) {
-        ++client_count;
+//        ++client_count;
+//        client_count.fetch_add(1);
+        client_count.fetch_add(1, std::memory_order_relaxed);
         std::cout << "add_client " << i << " : current client count = " << client_count << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
     std::cout << "add_client finished" << std::endl;
 }
 
-void work_operator(size_t& client_count) {
+void work_operator(std::atomic<size_t>& client_count) {
     std::cout << "work_operator start" << std::endl;
     while (client_count > 0) {
         std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-        --client_count;
+//        --client_count;
+//        client_count.fetch_sub(1);
+        client_count.fetch_sub(1, std::memory_order_relaxed);
         std::cout << "work_operator : current client count = " << client_count << std::endl;
     }
     std::cout << "work_operator finished" << std::endl;
@@ -25,7 +30,7 @@ void work_operator(size_t& client_count) {
 
 int main() {
     std::cout << "main start" << std::endl;
-    size_t client_count = 0;
+    std::atomic<size_t> client_count = 0;
     std::thread th_add_client{add_client, std::ref(client_count), 11};
     std::thread th_work_operator{work_operator, std::ref(client_count)};
     th_add_client.join();
